02baf5994a7be3cb9e23ab72f0b1efcbeed42ed6
Wesley Dias <wesley.dias.dev@gmail.com>
2022-09-22 08:23:50 -0500

    Finalizado a parte do usuário escolher uma letra, e se houver, ele mostrar na tela e marcar o botão com a letra

--- src/App.js
+++ src/App.js
@@ -13,24 +13,48 @@ function App() {
   let [palavraAtual, setPalavraAtual] = React.useState(palavras[Math.floor((Math.random()*200))]);
   let [botaoPalavra, setBotaoPalavra] = React.useState(false);
   let [tentativas, setTentativas] = React.useState([]);
-  let [error, setError] = React.useState(null)
-  console.log(botaoPalavra)
-
+  let [acertos, setAcertos] = React.useState([]);
+  let [error, setError] = React.useState([]);
+  console.log(palavraAtual)
   function mudaPalavra(){
     setBotaoPalavra(true)
     setPalavraAtual(palavras[Math.floor((Math.random()*200))]);
+    console.log(palavraAtual)
   }
 
-  function verificaLetra(){
-    if(palavraAtual.split('').f){
-
+  function verificaLetra(letra){
+    if(palavraAtual.includes(letra)){
+      setAcertos([...acertos, letra])
+      console.log('Acerto:',acertos)
+      return true
+    }else{
+      setError([...error, letra]);
+      console.log('Erro',error)
+      return false
     }
-    
   }
 
+  function trocaClasseLetra(letra){
+    if(acertos.includes(letra)){
+      return 'letraAcertou';
+    }else if(error.includes(letra)){
+      return 'letraErrou'
+    }else{
+      return 'letra'
+    }
+  }
+  
 
   const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   const imagens = [imagem0, imagem1, imagem2, imagem3, imagem4, imagem5, imagem6]
+
+  function palavraUsuario(letra){
+    if(acertos.includes(letra)){
+      return letra
+    }else{
+      return '_ '
+    }
+  }
 
   return (
     <div className='app'>
@@ -39,7 +63,7 @@ function App() {
         <div className='containerPalavra'>
           <button onClick={mudaPalavra} className='btnEscolhePergunta'>Escolher Palavra</button>
           <div>
-          {botaoPalavra ? palavraAtual.split('').map((l, index) => <span key={index} className='palavra'>_ </span>): ''}
+          {botaoPalavra ? palavraAtual.split('').map((l, index) => <span key={index} className='palavra'>{palavraUsuario(l)}</span>): ''}
           </div>
         </div>
       </div>
@@ -47,7 +71,7 @@ function App() {
 
         <div className='letrasCima'>{alfabeto.map((letra, index) => { 
           if(index < 13){
-            return <button className='letra' key={index}>{letra.toUpperCase()}</button>
+            return <button onClick={({target}) => verificaLetra(target.innerText.toLowerCase())} className={trocaClasseLetra(letra)} key={index}>{letra.toUpperCase()}</button>
           }
       })}</div>
 
@@ -55,7 +79,7 @@ function App() {
 
         {alfabeto.map((letra, index) => { 
           if(index >= 13){
-            return <button className='letra' key={index}>{letra.toUpperCase()}</button>
+            return <button onClick={({target}) => verificaLetra(target.value)} className={trocaClasseLetra(letra)} key={index}>{letra.toUpperCase()}</button>
           }
       })}
 
