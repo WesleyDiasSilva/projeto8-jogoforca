6ec407c32ad7c8bf7e4927515315cccacf2ef0b6
Wesley Dias <wesley.dias.dev@gmail.com>
2022-09-24 15:10:10 -0500

    Finalizada a parte de chute, onde o usuário pode tentar acertar a palavra correta através de um input

--- src/App.js
+++ src/App.js
@@ -12,24 +12,49 @@ function App() {
 
   let [palavraAtual, setPalavraAtual] = React.useState(palavras[Math.floor((Math.random()*200))]);
   let [botaoPalavra, setBotaoPalavra] = React.useState(false);
+  let [jogoFinalizado, setJogoFinalizado] = React.useState(false);
+  let [classFinalizado, setClassFinalizado] = React.useState('palavra');
   let [disableBtn, setDisableBtn] = React.useState(true);
   let [tentativas, setTentativas] = React.useState(0);
   let [acertos, setAcertos] = React.useState([]);
   let [error, setError] = React.useState([]);
+  let [valorInput, setValorInput] = React.useState('');
   console.log(palavraAtual)
+  
+ function alteraInput(target){
+  setValorInput(target)
+ }
+
 
   function mudaPalavra(){
     setBotaoPalavra(true)
+    setJogoFinalizado(false);
     setPalavraAtual(palavras[Math.floor((Math.random()*200))]);
     setDisableBtn(false)
     setAcertos([]);
     setError([]);
     setTentativas(0);
+    setValorInput('')
+  }
+
+
+
+  function finalizaJogo(result){
+    setJogoFinalizado(true);
+    setDisableBtn(true);
+    setBotaoPalavra(false);
+    if(result){
+      setClassFinalizado('palavra finalizadoGanhou')
+      return {class: 'ganhouJogo'}
+    }else{
+      setClassFinalizado('palavra finalizadoPerdeu')
+      return {class: 'perdeuJogo'}
+    }
   }
 
   function verificaLetra(target, letra){
     target.disabled = true
-    
+
     if(palavraAtual.includes(letra)){
       setAcertos([...acertos, letra])
       
@@ -37,6 +62,7 @@ function App() {
       setError([...error, letra]);
       setTentativas(tentativas +1)
     }
+
   }
 
   function trocaClasseLetra(letra){
@@ -49,7 +75,7 @@ function App() {
       return 'letra'
     }
   }
-  
+
 
   const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   const imagens = [imagem0, imagem1, imagem2, imagem3, imagem4, imagem5, imagem6]
@@ -65,14 +91,16 @@ function App() {
   return (
     <div className='app'>
       <div className='parteSuperior'> 
-        <img src= {imagens[tentativas]} alt='teste'/>     
+        <img src= {imagens[tentativas]} alt='teste'/>   
+          
         <div className='containerPalavra'>
           <button onClick={mudaPalavra} className='btnEscolhePergunta'>Escolher Palavra</button>
           <div>
-          {botaoPalavra ? palavraAtual.split('').map((l, index) => <span key={index} className='palavra'>{palavraUsuario(l)}</span>): ''}
+            {jogoFinalizado ? <span className={classFinalizado}>{palavraAtual}</span> : botaoPalavra ? palavraAtual.split('').map((l, index) => <span key={index} className='palavra'>{palavraUsuario(l)}</span>): ''}
           </div>
         </div>
       </div>
+
       <div className='letras'>
 
         <div className='letrasCima'>{alfabeto.map((letra, index) => { 
@@ -92,11 +120,13 @@ function App() {
 
         </div>
       </div>
+
       <div className='chute'>
         <label className='jaSei'>Já sei a palavra!</label>
-        <input type='text' disabled={disableBtn}/>
-        <button disabled={disableBtn} className='btnChutar'>Chutar</button>
+        <input value={valorInput} onChange={(e) => alteraInput(e.target.value)} type='text' disabled={disableBtn}/>
+        <button disabled={disableBtn} onClick={() => (valorInput.toLowerCase() === palavraAtual ? finalizaJogo(true) : finalizaJogo(false))} className='btnChutar'>Chutar</button>
       </div>
+
     </div>
   )
 }
